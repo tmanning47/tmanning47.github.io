8e27d5cdb2cbf5699ccb46736e8f9ccbe0a0bd0a
Plenum <lovecars47@gmail.com>
2024-08-11 12:24:27 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 function loadHeader() {
-    fetch('header.html')
+    fetch('nav.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('nav-placeholder').innerHTML = data;
