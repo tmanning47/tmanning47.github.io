a10e1dda6f9fbc036249fac6c2e5b2e32fa60f5a
Plenum <lovecars47@gmail.com>
2024-08-11 15:28:49 -0400

    Revert

--- nav.js
+++ nav.js
@@ -1,27 +1,12 @@
-function isMobileView() {
-    return window.innerWidth <= 840;
-}
-
 function loadHeader() {
-    fetch('nav.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('nav-placeholder').innerHTML = data;
-            setCurrentPage();
-            initializeDropdown();
-            initializeMobileNav();
-            // Check viewport size after a short delay
-            setTimeout(function() {
-                if (isMobileView()) {
-                    $('#titleBar, #navPanel').show();
-                    $('#header').hide();
-                } else {
-                    $('#titleBar, #navPanel').hide();
-                    $('#header').show();
-                }
-            }, 100);
-        });
-}
+                fetch('nav.html')
+                    .then(response => response.text())
+                    .then(data => {
+                        document.getElementById('nav-placeholder').innerHTML = data;
+                        setCurrentPage();
+                        initializeDropdown();
+                    });
+            }
 
             function setCurrentPage() {
                 const currentPath = window.location.pathname;
@@ -53,73 +38,4 @@ function loadHeader() {
                 });
             }
 
-function initializeMobileNav() {
-    const $body = $('body');
-    let $navPanel = $('#navPanel');
-    let $titleBar = $('#titleBar');
-
-    // If the elements don't exist, create them
-    if (!$navPanel.length) {
-        $navPanel = $('<div id="navPanel"><nav></nav></div>').appendTo($body);
-    }
-    if (!$titleBar.length) {
-        $titleBar = $('<div id="titleBar"><a href="#navPanel" class="toggle"></a><span class="title"></span></div>').appendTo($body);
-    }
-
-    // Update the content
-    let navHtml = '';
-    $('#nav > ul > li').each(function() {
-        let $this = $(this);
-        navHtml += '<a href="' + $this.find('> a').attr('href') + '">' + $this.find('> a').text() + '</a>';
-        
-        let $subMenu = $this.find('> ul');
-        if ($subMenu.length) {
-            $subMenu.find('> li').each(function() {
-                let $subItem = $(this);
-                navHtml += '<a href="' + $subItem.find('a').attr('href') + '" class="indent-1">' + $subItem.find('a').text() + '</a>';
-            });
-        }
-    });
-    
-    $navPanel.find('nav').html(navHtml);
-    $titleBar.find('.title').html($('#logo').html());
-
-    // Initialize the panel
-    $navPanel.panel({
-        delay: 500,
-        hideOnClick: true,
-        hideOnSwipe: true,
-        resetScroll: true,
-        resetForms: true,
-        side: 'left',
-        target: $body,
-        visibleClass: 'navPanel-visible'
-    });
-
-    // Re-bind click events for mobile nav
-    $titleBar.find('.toggle').off('click').on('click', function(e) {
-        e.preventDefault();
-        $body.toggleClass('navPanel-visible');
-    });
-
-    // Show/hide based on screen size
-    if (isMobileView()) {
-        $titleBar.show();
-        $navPanel.show();
-        $('#header').hide();
-    } else {
-        $titleBar.hide();
-        $navPanel.hide();
-        $('#header').show();
-    }
-}
-
-document.addEventListener('DOMContentLoaded', loadHeader);
-
-let resizeTimer;
-window.addEventListener('resize', function() {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function() {
-        initializeMobileNav();
-    }, 250);
-});
+            document.addEventListener('DOMContentLoaded', loadHeader);
