35cf0717045736eaf2ac2618542015839566a6d3
Plenum <lovecars47@gmail.com>
2024-08-11 10:01:53 -0400

    Header fix

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 
             function loadHeader() {
-                fetch('header.html')
+                fetch('nav.html')
                     .then(response => response.text())
                     .then(data => {
                         document.getElementById('nav-placeholder').innerHTML = data;
