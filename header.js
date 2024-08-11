b42507330d3e8a9086f3df232e92653a62592800
Plenum <lovecars47@gmail.com>
2024-08-11 01:53:34 -0400

    .js test

--- header.js
+++ header.js
@@ -1,3 +1,4 @@
+/*
 fetch('header.html')
 .then(res => res.text())
 .then(text => {
@@ -6,3 +7,19 @@ fetch('header.html')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
+*/
+async function loadHeader() {
+  try {
+    const response = await fetch('header.html');
+    const data = await response.text();
+    const headerContainer = document.getElementById('header-container');
+    headerContainer.innerHTML = data;
+
+    // Your dropdown menu logic here
+    // ...
+  } catch (error) {
+    console.error('Error fetching header:', error);
+  }
+}
+
+loadHeader();
