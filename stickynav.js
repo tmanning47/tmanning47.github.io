98afced104ac6343c8f1992af8e4f20c1d90804b
Plenum <lovecars47@gmail.com>
2024-08-13 19:12:17 -0400

    sticky testing

---
+++ stickynav.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    var header = document.getElementById('header');
+    var logo = document.getElementById('logo');
+    if (window.pageYOffset > logo.offsetHeight) {
+        header.style.top = '0';
+    } else {
+        header.style.top = '-100px'; // Should match the initial top value in CSS
+    }
+});
